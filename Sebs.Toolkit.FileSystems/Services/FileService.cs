dcbdb4fc1dad15cae248cda252dc536d45b47314
ToSeba <seb.developing@gmail.com>
2023-05-26 16:24:42 +0300

    Added File Service.

---
+++ Sebs.Toolkit.FileSystems/Services/FileService.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Sebs.FileSystems.Services
+{
+    /// <summary>
+    /// Purpose: Read system files ("mp3 | .pdf" etc)
+    /// Created by: sebde
+    /// Created at: 5/22/2023 10:58:20 PM
+    /// </summary>
+    public class FileService : IFileService
+    {
+        public async IAsyncEnumerable<string> GetItemsAsync(string path, string patterns,
+            [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            foreach (var pattern in patterns.Split('|'))
+            {
+                foreach (var file in Directory.EnumerateFiles(path, pattern,
+                    new EnumerationOptions
+                    {
+                        IgnoreInaccessible = true,
+                        RecurseSubdirectories = true,
+                        ReturnSpecialDirectories = false
+                    }))
+                {
+                    await Task.Delay(1, cancellationToken);
+                    yield return file;
+                }
+            }
+        }
+    }
+}
---
+++ Sebs.Toolkit.FileSystems/Services/IFileService.cs
@@ -0,0 +1,13 @@
+﻿namespace Sebs.FileSystems.Services
+{
+    /// <summary>
+    /// Purpose: Interface for fetching files from PC System
+    /// Created by: sebde
+    /// Created at: 5/22/2023 10:57:13 PM
+    /// </summary>
+    public interface IFileService
+    {
+        IAsyncEnumerable<string> GetItemsAsync(string path, string patterns, CancellationToken cancellationToken = default);
+    }
+}
+
